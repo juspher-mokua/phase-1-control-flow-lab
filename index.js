5d0739b9758cd04f3ca6f27ff4c66214930cfb32
Juspher Mokua <juspermokua6@gmail.com>
2022-08-17 00:58:44 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(sample){
+ if(sample<=400){
+  return 'This one is on me!';
+
+ } else if(sample > 2500) {
+return 'No can do.';
+ } else if(sample > 2000) {
+ return 'I will gladly take your thirty bucks.';
+ }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const findCity = city==="NYC"? "Ok, sounds good." : "No go."
+  return findCity;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case (tip = "generous"):
+      return  "Thank you so much.";
+      break;
+    case (tip = "not as generous"):
+      return  "Thank you.";
+      break;
+    default:  
+      return  'Bye.'; 
+  }
 }
